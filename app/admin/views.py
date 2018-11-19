3269ffc6744a43166e308051f484440fce300e5c
mathuryash5 <mathuryash5@gmail.com>
2018-11-19 18:14:11 +0530

    Updated DB data types

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,8 +1,9 @@
 # /app/admin/views.py
 
+
 # Import flask dependencies
 from flask import request, render_template, \
-      				session, redirect, url_for
+      				session, redirect, url_for, Response, jsonify
 
 from . import admin
 
@@ -12,26 +13,34 @@ from sqlalchemy import func, distinct
 
 from .. import db
 
+headers = "application/json"
 
 @admin.route('/dashboard', methods = ['GET', 'POST'])
 def dashboard():
 	print("Displaying Admin Dashboard")
-	student_count = db.session.query(func.count(Student.usn))
-	teacher_count = db.session.query(func.count(Teacher.f_id))
+	student_count = db.session.query(func.count(Student.usn)).first()
+	teacher_count = db.session.query(func.count(Teacher.f_id)).first()
 	# course_count = db.session.query(func.count(CourseBase.course_name))
-	teacher_names = db.session.query(Teacher.name)
-	sections = db.session.query(func.count(distinct(Student.section)))
+	teacher_names = db.session.query(Teacher.name).all()
+	sections = db.session.query(distinct(Student.section)).all()
 	# course_names = session.query(func.count(distinct(CourseBase.course_name)))
-	print(student_count, "Registered Students")
-	print(teacher_count, "Teachers")
-	# print(course_count, "Number of Courses")
-	print("Names of teachers", teacher_names)
 
-	return render_template('admin.html',student_count = student_count, 
-										teacher_count = teacher_count,
-										teacher_names = teacher_names,
-										sections = sections
-							)
+	teacher_list = []
+	for obj in teacher_names:
+		# print(type(obj))
+		teacher_list.append(obj[0])
+		teacher_list.sort()
+
+	section_list = []
+	for obj in sections:
+		# print(type(obj))
+		section_list.append(obj[0])
+		section_list.sort()
+
+	response = {"student_count" : student_count[0], "teacher_count" : teacher_count[0], "teacher_names" : teacher_list, "sections" : section_list}
+
+	# return str(response)
+	return render_template('admin.html', response = response)
 							#  ,
 							# 			course_names = course_names,
 							# 			course_count = course_count,				
--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@
 ## Check if a student has a team.
 ## Calendar ID with the course ID 
 ## How do we store the invites sent to a team mate?
-
+## Have timestamp associated with project eval to store the feedback from the teacher.
 
 from sqlalchemy.sql import func
 
@@ -37,7 +37,7 @@ class Student(User):
 	section = db.Column(db.String(2), nullable = False)
 	# A key value pair where the key would the course id and the value denotes team_id
 	has_team = db.Column(db.JSON) 
-	marks = db.Column(db.JSON)
+	lab_marks = db.Column(db.JSON)
 
 	__mapper_args__ = {
 		'polymorphic_identity': 'student',
@@ -60,6 +60,7 @@ class Teacher(User):
 	
 	f_id = db.Column(db.String(128), primary_key = True)
 	position = db.Column(db.String(128))
+	# 
 	course_to_section = db.Column(db.JSON)
 
 	__mapper_args__ = {
@@ -77,85 +78,89 @@ class Teacher(User):
 	def __repr__(self):
 		return '<TeacherProfile {}>'.format(self.name)
 
-# class CourseBase(db.Model):
-# 	__tablename__ = "course_base"
+class CourseBase(db.Model):
+	__tablename__ = "course_base"
 
-# 	course_name = db.Column(db.String, primary_key = True)
-# 	course_id = db.Column(db.Integer, unique = True)
-# 	description = db.Column(db.Text, nullable = False)
-# 	semester = db.Column(db.Integer)
-# 	# The constraint on the max number of team members.
-# 	no_of_max_members = db.Column(db.Integer)
-# 	calendar_id = db.Column(db.String(128))
-# 	teams = db.relationship('Team', backref = 'course', lazy = True)
+	course_name = db.Column(db.String, primary_key = True)
+	course_code = db.Column(db.Integer, unique = True, primary_key = True )
+	description = db.Column(db.Text, nullable = False)
+	semester = db.Column(db.Integer)
+	# The constraint on the max number of team members.
+	no_of_max_members = db.Column(db.Integer)
+	calendar_id = db.Column(db.String(128))
+	# teams = db.relationship('Team', backref = 'course', lazy = True)
 
-# 	def __init__(self, course_name, course_id, description, semester, no_of_max_members, calendar_id):
-# 		self.course_name = course_name
-# 		self.course_id = course_id
-# 		self.description = description
-# 		self.semester = semester
-# 		self.no_of_max_members = no_of_max_members
-# 		## Add the calendar initialization code here
-# 		self.calendar_id = calendar_id
+	def __init__(self, course_name, course_code, course_id, description, semester, no_of_max_members, calendar_id):
+		self.course_name = course_name
+		self.course_code = course_code
+		self.course_id = course_id
+		self.description = description
+		self.semester = semester
+		self.no_of_max_members = no_of_max_members
+		## Add the calendar initialization code here
+		self.calendar_id = calendar_id
 
-# 	def __repr__(self):
-# 		return '<CourseBase {}>'.format(self.course_name)
-
-
-# class Course(db.Model):
-# 	__tablename__ = "course"
-
-# 	course_id = db.Column(db.Integer, db.ForeignKey('coursebase.course_id'))
-# 	deliverable_id = db.Column(db.Integer, nullable = False, primary_key = True)
-# 	deliverables = db.Column(db.String(256))
-# 	deliverable_deadline = db.Column(db.DateTime(timezone = True))
-# 	# Resources supplied by the teacher for the students. 
-# 	teacher_resources = db.Column(db.JSON)
-
-# 	def __init__(self, course_id, deliverable_id, deliverables, deliverable_deadline, teacher_resources):
-# 		self.course_id = course_id
-# 		self.deliverable_id = deliverable_id
-# 		self.deliverables = deliverables
-# 		self.deliverable_deadline = deliverable_deadline
-# 		self.teacher_resources = teacher_resources
-
-# 	def __repr__(self):
-# 		return '<Course {}>'.format(self.course_name)
+	def __repr__(self):
+		return '<CourseBase {}>'.format(self.course_name)
 
 
-# class Team(db.Model):
-# 	__tablename__ = "team"
+class Course(db.Model):
+	__tablename__ = "course"
 
-# 	team_id = db.Column(db.Integer, primary_key = True)
-# 	course_id = db.Column(db.Integer, db.ForeignKey('coursebase.course_id'))
-# 	# IDs required for chat sessions.
-# 	session_id = db.Column(db.Integer, nullable = False)
-# 	usn_list = db.Column(db.JSON)
-# 	github_user = db.Column(db.String(64))
-# 	github_repo = db.Column(db.String(128))
+	course_code = db.Column(db.Integer, db.ForeignKey('coursebase.course_code'), primary_key = True)
+	deliverable_id = db.Column(db.Integer, nullable = False, primary_key = True)
+	deliverables = db.Column(db.String(256))
+	deliverable_deadline = db.Column(db.DateTime(timezone = True))
+	# Resources supplied by the teacher for the students. 
+	teacher_resources = db.Column(db.JSON)
 
-# 	def __init__(self, team_id, course_id, session_id, usn_list, github_user, github_repo):
-# 		self.team_id = team_id
-# 		self.course_id = course_id
-# 		self.session_id = session_id
-# 		self.usn_list = usn_list
-# 		self.github_user = github_user
-# 		self.github_repo = github_repo
+	def __init__(self, course_id, deliverables, deliverable_deadline, teacher_resources):
+		self.course_id = course_id
+		# self.deliverable_id = deliverable_id
+		self.deliverables = deliverables
+		self.deliverable_deadline = deliverable_deadline
+		# self.teacher_resources = teacher_resources
 
-# # class TeamSubmissions(db.Model):
-# # 	__tablename__ = "team_submissions"
+	def __repr__(self):
+		return '<Course {}>'.format(self.course_name)
 
-# # 	team_id = db.Column(db.Integer, db.ForeignKey('team.team_id'))
-# # 	deliverable_id = db.Column(db.Integer)
-# # 	resource_url = db.Column(db.String(128))
-# # 	grading_status = db.Column(db.Boolean, default = True)
-# # 	submission_time = db.Column(db.DateTime(timezone = True))
+
+class Team(db.Model):
+	__tablename__ = "team"
+
+	team_id = db.Column(db.Integer, primary_key = True)
+	# , autoincrement = True)
+	course_code = db.Column(db.String, db.ForeignKey('coursebase.course_code'), primary_key = True)
+	# IDs required for chat sessions.
+	session_id = db.Column(db.Integer, nullable = False, autoincrement =  True)
+	usn_list = db.Column(db.JSON)
+	github_user = db.Column(db.String(64))
+	github_repo = db.Column(db.String(128))
+	marks = db.Column(db.Integer)
+
+	def __init__(self, team_id, course_id, session_id, usn_list, github_user, github_repo):
+		self.team_id = team_id
+		self.course_id = course_id
+		self.session_id = session_id
+		self.usn_list = usn_list
+		self.github_user = github_user
+		self.github_repo = github_repo
+
+class TeamSubmissions(db.Model):
+	__tablename__ = "team_submissions"
+
+	team_id = db.Column(db.Integer, db.ForeignKey('team.team_id'), primary_key = True)
+	course_code = db.Column(db.String, db.ForeignKey('course_base.course_code'), primary_key = True)
+	deliverable_id = db.Column(db.Integer, primary_key = True)
+	resource_url = db.Column(db.String(128))
+	grading_status = db.Column(db.Boolean, default = True)
+	submission_time = db.Column(db.DateTime(timezone = True))
 
 
 
 # # @login_manager.user_loader
 # # def load_user(id):
-# # 	try:
+# # 	try: 
 # # 		return User.query.get(int(id))
 # # 	except:
 # # 		return None
