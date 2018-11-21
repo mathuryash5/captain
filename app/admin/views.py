546be21846e53f94d2d4955f52c4331ae186f4b5
mathuryash5 <mathuryash5@gmail.com>
2018-11-21 19:34:53 +0530

    Student and teacher list integrated on the Admin page

--- app/admin/views.py
+++ app/admin/views.py
@@ -7,7 +7,7 @@ from flask import request, render_template, \
 
 from . import admin
 
-from app.models import Student, Teacher
+from app.models import Student, Teacher, CourseBase
 
 from sqlalchemy import func, distinct
 
@@ -20,24 +20,30 @@ def dashboard():
 	print("Displaying Admin Dashboard")
 	student_count = db.session.query(func.count(Student.usn)).first()
 	teacher_count = db.session.query(func.count(Teacher.f_id)).first()
-	# course_count = db.session.query(func.count(CourseBase.course_name))
+	course_count = db.session.query(func.count(CourseBase.course_name)).first()
 	teacher_names = db.session.query(Teacher.name).all()
 	sections = db.session.query(distinct(Student.section)).all()
-	# course_names = session.query(func.count(distinct(CourseBase.course_name)))
+	course_names = db.session.query(CourseBase.course_name).all()
 
 	teacher_list = []
 	for obj in teacher_names:
 		# print(type(obj))
 		teacher_list.append(obj[0])
-		teacher_list.sort()
+	teacher_list.sort()
 
 	section_list = []
 	for obj in sections:
 		# print(type(obj))
 		section_list.append(obj[0])
-		section_list.sort()
+	section_list.sort()
 
-	response = {"student_count" : student_count[0], "teacher_count" : teacher_count[0], "teacher_names" : teacher_list, "sections" : section_list}
+	course_list = []
+	for obj in course_names:
+		course_list.append(obj[0])
+	course_list.sort()
+
+	response = {"student_count" : student_count[0], "course_count" : course_count[0], "teacher_count" : teacher_count[0],
+				 "teacher_names" : teacher_list, "sections" : section_list, "course_names" : course_list}
 
 	# return str(response)
 	return render_template('admin.html', response = response)
@@ -57,9 +63,11 @@ def students():
 	print("Displaying Students Page for the Admin")
 	student_info = db.session.query(Student).filter(Student.role == 'Student').all()
 	print(student_info)
-	return render_template('studentlist.html')
+	return render_template('studentlist.html', response = student_info)
 
 @admin.route('/teachers', methods = ['GET', 'POST'])
 def teachers():
 	print("Displaying Teachers Page for the Admin")
-	return render_template('teacherlist.html')
+	teacher_info = db.session.query(Teacher).filter(Teacher.role == 'Teacher').all()
+	print(teacher_info)
+	return render_template('teacherlist.html', response = teacher_info)
