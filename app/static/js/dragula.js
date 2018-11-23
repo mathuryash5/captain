d4c305777a0b089a56b665a01c54ac1a4ef4f59a
varuni <varuni@varuni.Dlink>
2018-11-23 12:37:48 +0530

    tabbed course

--- app/static/js/dragula.js
+++ app/static/js/dragula.js
@@ -2,9 +2,16 @@
     'use strict';
     var iconTochange;
    
-    dragula([document.getElementById("profile-list-all1"), document.getElementById("profile-list-subj1"),document.getElementById("profile-list-subj2"),document.getElementById("profile-list-subj3")], {
+    dragula([document.getElementById("profile-list-all1"), document.getElementById("profile-list-subj1")], {
+
+        removeOnSpill: true,
+         
+       
+        
+      });
+      dragula([document.getElementById("profile-list-all2"), document.getElementById("profile-list-subj2")], {
         copy: function (el, source) {
-          return source === document.getElementById("profile-list-all1")
+          return source === document.getElementById("profile-list-all2")
         },
         removeOnSpill: true,
          verify: function(el,target)
@@ -17,7 +24,25 @@
          }, 
        
         accepts: function (el, target) {
-          return target !== document.getElementById("profile-list-all1")
+          return target !== document.getElementById("profile-list-all2")
+        }
+      });
+      dragula([document.getElementById("profile-list-all3"), document.getElementById("profile-list-subj3")], {
+        copy: function (el, source) {
+          return source === document.getElementById("profile-list-all3")
+        },
+        removeOnSpill: true,
+         verify: function(el,target)
+         {
+           if(global > 0)
+           {
+            return removeOnSpill = false
+          }
+
+         }, 
+       
+        accepts: function (el, target) {
+          return target !== document.getElementById("profile-list-all3")
         }
       });
   
